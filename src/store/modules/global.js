454286826c76513d50216d8c976b86301c80a3e7
Yoohoo <yao460733480@outlook.com>
2018-01-08 20:56:29 +0800

    change to use domain instead of ip for user login

--- src/store/modules/global.js
+++ src/store/modules/global.js
@@ -14,7 +14,7 @@ const globalModule = {
     async getUserInfo() {
       const res = await api.getInfo();
       if (res.code === 10030) {
-        location.assign('http://112.74.25.233:8004/app/login/wx-code');
+        location.assign('http://ghw.work2pix.top/app/login/wx-code');
       } else {
       }
     },
