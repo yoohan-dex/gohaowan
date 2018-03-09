93ad21df200e9efc7b47f9cf9f7c138ded4f0015
Yoohoo <yao460733480@outlook.com>
2018-03-09 20:11:46 +0800

    share production

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   // IMAGE_PREFIX: '"http://go.ghw123.com"',
-  IMAGE_PREFIX: '"http://ghw.work2pix.top"',
+  IMAGE_PREFIX: '"https://ghw123.com"',
 };
