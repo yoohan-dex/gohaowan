642ef2cebe263b1253861bce54f2240423cfe65e
Yoohoo <yao460733480@outlook.com>
2018-03-14 15:58:30 +0800

    test the redirect work

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env');
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"http://ghw.work2pix.top"',
+  REDIRECT_PATH: '"http://ghw.work2pix.top/app/login/auth?redirect="',
   IMAGE_PREFIX: '"http://ghw.work2pix.top"',
 });
--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,6 @@ module.exports = {
   NODE_ENV: '"production"',
   // IMAGE_PREFIX: '"http://go.ghw123.com"',
   BASE_API: '"http://ghw.work2pix.top"',
+  REDIRECT_PATH: '"http://ghw.work2pix.top/app/login/auth?redirect="',
   IMAGE_PREFIX: '"https://ghw123.com"',
 };
