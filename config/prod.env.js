a02fdb72bb773dcc1f72fdd47112ae891b4f2313
Yoohoo <yao460733480@outlook.com>
2018-03-19 19:59:06 +0800

    change prod.env

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   // IMAGE_PREFIX: '"http://go.ghw123.com"',
-  BASE_API: '"http://ghw.work2pix.top"',
-  REDIRECT_PATH: '"http://ghw.work2pix.top/app/login/auth?redirect="',
+  // BASE_API: '"http://ghw.work2pix.top"',
+  REDIRECT_PATH: '"http://ghw123.com/app/login/auth?redirect="',
   IMAGE_PREFIX: '"https://ghw123.com"',
 };
