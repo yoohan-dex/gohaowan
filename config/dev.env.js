76d1fe054f3e0e576b84085a01da9750dbd09df1
Yoohoo <yao460733480@outlook.com>
2018-03-20 18:08:22 +0800

    fixed headimg shape and the follow throttle

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ const prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // BASE_API: '"http://ghw.work2pix.top"',
+  BASE_API: '"https://ghw123.com"',
   REDIRECT_PATH: '"http://ghw.work2pix.top/app/login/auth?redirect="',
   IMAGE_PREFIX: '"http://ghw.work2pix.top"',
 });
